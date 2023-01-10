50d302c4fb0ea2cdd72ab4a076028e167079fba8
Partab Rai <amrani.partab@gmail.com>
2023-01-10 23:20:46 +0500

    huge bug fixes

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -4,15 +4,6 @@ import { useRouter } from 'next/router';
 import { getCategories, getCategoryPost } from '../../services';
 import { PostCard, Categories, Loader } from '../../components';
 
-import { motion } from 'framer-motion';
-
-const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
-}
-
-
 const CategoryPost = ({ posts }) => {
   const router = useRouter();
 
@@ -21,17 +12,6 @@ const CategoryPost = ({ posts }) => {
   }
 
   return (
-    <motion.main
-    variants={variants} // Pass the variant object into Framer Motion 
-    initial="hidden" // Set the initial state to variants.hidden
-    animate="enter" // Animated state to variants.enter
-    exit="exit" // Exit state (used later) to variants.exit
-    transition={{ type: 'linear' }} // Set the transition to linear
-    className=""
->
-<Head>
-          <title>Pak Edu | Categories</title>
-        </Head>
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
@@ -46,7 +26,6 @@ const CategoryPost = ({ posts }) => {
         </div>
       </div>
     </div>
-    </motion.main>
   );
 };
 export default CategoryPost;
